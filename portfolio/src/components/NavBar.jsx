06d3b63118947dabd781fa06fd8bc110f93bcfd9
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2024-01-10 21:28:46 -0800

    Fix: GH-pages displaying wrong path

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { navLinks } from "../constants/navconsts";
-import logo from "../assets/logo.png";
+import React from "react"
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import { navLinks } from "../constants/navconsts"
+import logo from "../assets/logo.png"
 
 const NavBar = () => {
-  const [transition, setTransition] = useState(false);
+  const [transition, setTransition] = useState(false)
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setTransition(true);
-    }, 10);
-    return () => clearTimeout(timeout);
-  }, []);
+      setTransition(true)
+    }, 10)
+    return () => clearTimeout(timeout)
+  }, [])
   return (
     <nav className="bg-black w-full flex py-3 justify-between items-center navbar">
       <div
@@ -19,7 +19,7 @@ const NavBar = () => {
           transition ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
         } transition ease-in-out`}
       >
-        <a className="mb-5" href="/">
+        <a className="mb-5" href="https://kevinxiao27.github.io/">
           <img className="object-contain h-16 ml-5" src={logo} alt="Logo" />
         </a>
       </div>
@@ -41,7 +41,7 @@ const NavBar = () => {
         ))}
       </ul>
     </nav>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
