f4999984de544ae4cb623d3fb2b15010baf55e13
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2024-01-10 21:06:43 -0800

    Feat: Add experience component  and animations

--- portfolio/src/components/Experience.jsx
+++ portfolio/src/components/Experience.jsx
@@ -1,8 +1,67 @@
-import React from "react";
-import transition from "../utils/transition";
+import React from "react"
+import { useState, useEffect } from "react"
+import { useLocation } from "react-router"
+import { experience } from "../constants/experience"
+import transition from "../utils/transition"
 
 const Experience = () => {
-  return <div>Experience</div>;
-};
+  const location = useLocation()
+  const [animation, setAnimation] = useState(false)
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAnimation(true)
+    }, 10)
+    return () => clearTimeout(timeout)
+  }, [location])
+  return (
+    <>
+      <div className="bg-black w-2/4 border-solid border-2 border-gray-100 border-t-0 border-l-0 p-3 flex mt-4 mx-auto">
+        <h1 className="mx-auto mttext-xl font-bold">Experience</h1>
+      </div>
+      <div className="container bg-transparent mx-auto w-full h-full">
+        <div className="relative wrap overflow-hidden p-10 h-full">
+          <div
+            className="border-2-2 absolute border-opacity-20 border-white h-full border"
+            style={{ left: "50%" }}
+          ></div>
+          {experience.map((exp, index) => {
+            const displaySide = index % 2 === 1 ? "flex-row-reverse" : ""
+            const directionIn =
+              index % 2 === 0 ? "translate-x-36" : "-translate-x-36"
+            return (
+              <div
+                className={`mb-8 flex ${displaySide} justify-between items-center w-full right-timeline`}
+              >
+                <div className="order-1 w-5/12"></div>
+                <div className="flex items-center order-1 bg-black border-solid border-2 border-white border-l-0 border-t-0 shadow-xl w-8 h-8 rounded-full">
+                  <h1 className="mx-auto font-semibold text-lg text-white ">
+                    {index + 1}
+                  </h1>
+                </div>
+                <div
+                  className={`order-1 bg-gray-900 border-solid border-2 border-t-0 border-l-0 border-white rounded-lg shadow-xl w-5/12 px-6 py-4 duration-1000 ${
+                    animation ? "opacity-100 translate-x-0" : directionIn
+                  } transition ease-in-out`}
+                >
+                  <h3 className="font-bold text-white text-xl">{exp.name}</h3>
+                  <div className="flex">
+                    <h3 className="mb-3 font-normal text-white text-xl flex-none">
+                      <a href={exp.link}>{exp.organization}</a>
+                    </h3>
+                    <div className="grow"></div>
+                    <h3 className="flex-none">{`${exp.startDate} -- ${exp.endDate}`}</h3>
+                  </div>
+                  <p className="text-sm leading-snug tracking-wide text-white text-opacity-100">
+                    {exp.description}
+                  </p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  )
+}
 
-export default transition(Experience);
+export default transition(Experience)
--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,65 +1,97 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { useState, useEffect } from "react";
-import { motion } from "framer-motion";
-import AboutMe from "./AboutMe";
-import Pyramid3d from "../utils/3dPyramid";
-import transition from "../utils/transition";
-import arrowleft from "../assets/arrowleft.png";
+import React from "react"
+import { Canvas } from "@react-three/fiber"
+import { useState, useEffect } from "react"
+import profile from "../assets/profile-picture.png"
+import { motion } from "framer-motion"
+import Pyramid3d from "../utils/3dPyramid"
+import transition from "../utils/transition"
+import arrowleft from "../assets/arrowleft.png"
 
 const Home3d = () => {
-  const [transition, setTransition] = useState(false);
+  const [transition, setTransition] = useState(false)
   useEffect(() => {
-    setTransition(true);
-  }, []);
+    setTransition(true)
+  }, [])
   return (
     <>
-      <section className="Home-Display">
-        <div
-          className={`mx-auto duration-1000 ${
-            transition
-              ? "opacity-100 translate-y-0"
-              : "opacity-0 translate-y-10"
-          } transition ease-slow`}
-        >
-          <div className="flex flex-row mt-10">
-            <div className="mx-auto">
-              <h1 className="translate-x-10 z-15 font-bold">Hi I'm Kevin!</h1>
-              <h1 className="translate-x-10 z-15 font-bold">
-                I develop software.
-              </h1>
-            </div>
-            <div className="w-5/12">
-              <div className="flex flex-row">
-                <div className="container flex-none w-64 h-32">
-                  <Canvas>
-                    <pointLight position={[0, 2, 2]} />
-                    {/* <ambientLight /> */}
-                    <Pyramid3d position={[1, 0, 0]} />
-                  </Canvas>
-                </div>
-                <div className="relative top-4 -translate-x-10 -translate-y-10 mt-4">
-                  <h2 className="absolute top-3 left-2 font-bold">Click Me!</h2>
-                  <img
-                    className="brightness-200 w-24"
-                    src={arrowleft}
-                    alt="point-right arrow"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
       <div
         className={`ml-10 p-3 duration-1000 ${
           transition ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-10"
         } transition ease-slow`}
       >
-        <AboutMe />
+        <section className="Home-Display">
+          <div
+            className={`mx-auto duration-1000 ${
+              transition
+                ? "opacity-100 translate-y-0"
+                : "opacity-0 translate-y-10"
+            } transition ease-slow`}
+          >
+            <div
+              className={`flex flex-row flex-wrap mx-auto w-9/12 mt-10 mb-8 justify-between ${
+                transition
+                  ? "opacity-100 translate-y-0"
+                  : "opacity-0 translate-y-10"
+              } transition ease-slow duration-1000`}
+            >
+              <div className="mx-auto">
+                <h1 className="z-15 font-bold">Hi I'm Kevin!</h1>
+                <h1 className="z-15 font-bold">I develop software.</h1>
+              </div>
+              <div className="w-5/12 grow">
+                <div className="flex flex-row hidden lg:flex">
+                  <div className="container flex-none w-3/12 h-32">
+                    <Canvas>
+                      <pointLight position={[0, 2, 2]} />
+                      {/* <ambientLight /> */}
+                      <Pyramid3d position={[1, 0, 0]} />
+                    </Canvas>
+                  </div>
+                  <div className="relative top-4 -translate-x-10 -translate-y-10 mt-4">
+                    <h2 className="absolute top-3 left-2 font-bold">
+                      Click Me!
+                    </h2>
+                    <img
+                      className="brightness-200 w-24"
+                      src={arrowleft}
+                      alt="point-right arrow"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <div className="flex flex-row flex-wrap m-auto w-9/12 mt-3 mb-10 grid-cols-2">
+          <div className="bg-slate-700 p-2 flex-none">
+            <div className="w-96">
+              <img
+                className="-translate-x-6 -translate-y-6 w-96"
+                src={profile}
+              ></img>
+            </div>
+          </div>
+          <div className="p-8 bg-zinc-950 w-1/3 grow">
+            <p className="text-xl font-bold mb-4 ">
+              Learning is my passion. Software design and programming come as a
+              natural translation of my interests for systematic thinking.{" "}
+            </p>
+            <p>
+              I am a student @{" "}
+              <a
+                className="text-white"
+                href="https://mybcom.sauder.ubc.ca/bucs"
+              >
+                UBC
+              </a>{" "}
+              studying Business and Computer Science. I was born in Vancouver
+              but have lived in Calgary for most of my life.
+            </p>
+          </div>
+        </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default transition(Home3d);
+export default transition(Home3d)
