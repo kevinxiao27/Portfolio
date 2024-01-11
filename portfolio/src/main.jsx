3953b6fe1e664c931f2b336e142fe4b31c7df69c
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2024-01-10 21:37:12 -0800

    Fix: switched BrowserRouter for HashRouter to match GH Pages deployment

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,15 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./index.css";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { createHashRouter, RouterProvider } from "react-router-dom"
+import App from "./App.jsx"
+import "./index.css"
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
+    <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
